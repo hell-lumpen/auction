57d892b79526b0f090dd263ce53acdbe2486c4a5
Danila <danila041202@gmail.com>
2022-03-13 20:21:02 +0300

    add Say_gey function

--- auction-back/src/main/java/com/example/auctionback/controllers/SayHello.java
+++ auction-back/src/main/java/com/example/auctionback/controllers/SayHello.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping()
 public class SayHello {
-
+    @GetMapping("/gey")
+    public String say_gey() {return "Oleg gey";}
     @GetMapping()
     public String say_hello() {
         return "Hello world";
